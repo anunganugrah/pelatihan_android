297ecd00abec2f57c0f6633e0a904caddb7e2225
anunganugrah <anugrahsumardikoaas@yahoo.com>
2017-02-02 08:17:51 +0700

    Kalkulator

--- JavaClass/app/src/main/java/com/example/d2j_00/javaclass/MainActivity.java
+++ JavaClass/app/src/main/java/com/example/d2j_00/javaclass/MainActivity.java
@@ -11,39 +11,52 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showText = (TextView) findViewById(R.id.show_text);
 
-        showString =" Manipulasi Java Android";
-        addSeparator();
+
+
+
+        showString ="Manipulasi Java android";
+        addseparator();
 
 
         initbarang();
-        showString += arrbarang[5].toString();
-        addSeparator();
-        showString += arrbarang[1].toString();
+       // showString += arrbarang[5].toString();
+        //addseparator();
+       //showString += arrbarang[1].toString();
+       // showText.setText(showString);
+
+        transaksi trans1 = new transaksi();
+        trans1.addBarang(arrbarang[3]);
+        trans1.addBarang(arrbarang[7]);
+        trans1.addBarang(arrbarang[1]);
+        showString +=trans1.printTransaksi();
+        showString += "rata-rata harga barang yang dibeli :"+
+                trans1.averageTransaksi() ;
+        showString +=trans1.maxBarang();
         showText.setText(showString);
-    }
-    public void addSeparator(){
-        showString +="\n-----------------------\n";
+
     }
 
-    public  void initbarang(){
-        arrbarang[0] = new barang("Leptop", 1, 7000000);
-        arrbarang[1] = new barang("Printer",barang.ELEKTRONIK, 2500000);
-        arrbarang[2] = new barang("Monitor",barang.ELEKTRONIK, 1500000);
-        arrbarang[3] = new barang("Scanner",barang.ELEKTRONIK, 1000000);
-        arrbarang[4] = new barang("Meja",barang.NEW_EKTRONIK, 1500000);
-        arrbarang[5] = new barang("Kursi",barang.NEW_EKTRONIK, 2500000);
-        arrbarang[6] = new barang("Lemari",barang.NEW_EKTRONIK, 4500000);
-        arrbarang[7] = new barang("Megaphone",barang.ELEKTRONIK, 1500000);
-        arrbarang[8] = new barang("Tikar",barang.NEW_EKTRONIK, 150000);
-        arrbarang[9] = new barang("Sound system",barang.ELEKTRONIK, 2200000);
+
+    public void addseparator(){
+        showString +="\n------------------\n";
+    }
 
 
-
+    public void initbarang(){
+        arrbarang [0] = new barang("Laptop",1,70000000);
+        arrbarang [1] = new barang("Printer",barang.ELEKTRONIK,2500000);
+        arrbarang [2] = new barang("Monitor",barang.ELEKTRONIK,1000000);
+        arrbarang [3] = new barang("Scanner",barang.ELEKTRONIK,2000000);
+        arrbarang [4] = new barang("Handphone",barang.ELEKTRONIK,1500000);
+        arrbarang [5] = new barang("PC",barang.ELEKTRONIK,150000);
+        arrbarang [6] = new barang("Ces Laptop",barang.ELEKTRONIK,500000);
+        arrbarang [7] = new barang("Speaker",barang.ELEKTRONIK,15000000);
+        arrbarang [8] = new barang("Camera",barang.ELEKTRONIK,2500000);
+        arrbarang [9] = new barang("Kipas angin",barang.ELEKTRONIK,50000);
 
     }
 }
--- JavaClass/app/src/main/java/com/example/d2j_00/javaclass/barang.java
+++ JavaClass/app/src/main/java/com/example/d2j_00/javaclass/barang.java
@@ -4,46 +4,46 @@ package com.example.d2j_00.javaclass;
  * Created by D2J-00 on 31/01/2017.
  */
 public class barang {
-    public  static final int ELEKTRONIK =1;
-    public  static final int NEW_EKTRONIK =2;
+    public static final  int ELEKTRONIK =1;
+    public static final  int NEW_ELEKTRONIK =2;
     private String nama;
     private int category;
     private int harga;
 
-    public barang(String nama, int category, int harga){
+    public barang(String nama,int category,int harga){
         this.nama = nama;
         this.setCategory(category);
         this.harga = harga;
     }
 
     public String toString(){
-        return nama+" |"+this.getStrinCategory()+" |"+this.harga;
+        return nama+" |"+this.getStringCategory()+" |"+this.harga +"\n";
     }
 
-
-
-    public void  setNama(String nama){
+    public void setNama(String nama){
         this.nama = nama;
     }
     public String getNama(){
         return this.nama;
+
     }
 
     public int getCategory() {
         return category;
     }
-    public String getStrinCategory(){
-        if (category ==1)
-            return  "Elektronik";
+    public String getStringCategory(){
+    if (category==1)
+        return "elektronik";
         else
-            return "non Elekteronik";
+        return "non elektronik";
+
     }
 
     public void setCategory(int category) {
         if (category !=1 && category !=2)
-        this.category = 2;
+            this.category= 2;
         else
-            this.category = category;
+        this.category = category;
     }
 
     public int getHarga() {
